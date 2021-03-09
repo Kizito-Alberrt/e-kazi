a45da7a309d8d7fad39fac32a3955e9ff6a6366a
Kizito-Alberrt <wagabi98@gmail.com>
2021-03-09 14:33:18 +0300

    modified app,py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template,request,session,logging,url_for,redirect,flash
+ 
 
 
 
 app = Flask(__name__)
+
 @app.route("/")
-def index():
+def home():
   
-    return 'e-kazi'
+    return 'e-kazi your job solution'
 
 
 if __name__ == "__main__":
--- run.py
+++ run.py
@@ -1,11 +1,14 @@
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "<hi>Welcome to e-Kazi</h1>"
+# from flask import Flask, render_template,request,session,logging,url_for,redirect,flash
 
 
-if __name__ == '__main__':
-    app.run(debug =True)
+
+# app=Flask(__name__,template_folder='templates')
+
+# @app.route("/")
+# def index():
+  
+#     return render_template('index.html')
+
+
+# if __name__ == "__main__":
+#     app.run(debug =True)
